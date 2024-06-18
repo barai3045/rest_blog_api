cfd3de214f4d309f5decf3c8a05fd9e379ea7eb6
Santosh Barai <barai3022@gmail.com>
2024-06-18 22:48:02 +0600

    part04

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,11 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def blog_list(request):
+    data = {
+        "Message": "Hello World"
+    }
+    
+    return JsonResponse(data)
