457dc94f471417f9ff85dc007f6bc973ba157d38
Santosh Barai <barai3022@gmail.com>
2024-06-19 10:46:17 +0600

    part07

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,11 +1,26 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 
+from .models import Blog
+
 
 # Create your views here.
 def blog_list(request):
+    blogs = Blog.objects.all()
     data = {
-        "Message": "Hello World"
+        "Blogs": list(blogs.values())
+    }
+    
+    return JsonResponse(data)
+
+
+def blog_detail(request, pk):
+    blogs = Blog.objects.get(pk=pk)
+    
+    data = {
+        'name': blogs.name,
+        'description': blogs.description,
+        'slug': blogs.slug,
     }
     
     return JsonResponse(data)
--- settings/settings.py
+++ settings/settings.py
@@ -76,10 +76,16 @@ WSGI_APPLICATION = 'settings.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rest_blog_api',
+        'USER': 'barai',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
+    
 }
 
 
